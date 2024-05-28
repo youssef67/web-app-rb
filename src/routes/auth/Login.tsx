8fa45223c9b04ff0368f769ede7f164ded657825
youssef PC Bureau <you.moudni@gmail.com>
2024-05-29 00:55:29 +0200

    ajout de la appBar - 2 versions - avant/après login

--- src/routes/auth/Login.tsx
+++ src/routes/auth/Login.tsx
@@ -1,29 +1,38 @@
-import React from "react";
-import LoginForm from "@components/auth/forms/LoginForm";
-import Cookies from "js-cookie";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import LoginForm from '@components/auth/forms/LoginForm'
+import AppBarBeforeLogin from '@components/common/AppBarBeforeLogin'
+import Cookies from 'js-cookie'
+import { Grid } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const Login: React.FC = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate()
 
-  const allCookies = Cookies.get("compagnyUser");
-  console.log(allCookies);
+    const allCookies = Cookies.get('compagnyUser')
+    console.log(allCookies)
 
-  const handleRegister = () => {
-    navigate("/register");
-  };
+    const handleRegister = () => {
+        navigate('/register')
+    }
 
-  const handlePasswordForgottent = () => {
-    navigate("/password-forgotten");
-  };
-  return (
-    <>
-      <div>Login</div>
-      <LoginForm />
-      <button onClick={handleRegister}>Pas encore de compte</button>
-      <button onClick={handlePasswordForgottent}>mot de passe oublié</button>
-    </>
-  );
-};
+    const handlePasswordForgottent = () => {
+        navigate('/password-forgotten')
+    }
+    return (
+        <>
+            <Grid container>
+                <AppBarBeforeLogin/>
+                <Grid item>
+                    <div>Login</div>
+                </Grid>
+                <LoginForm />
+                <button onClick={handleRegister}>Pas encore de compte</button>
+                <button onClick={handlePasswordForgottent}>
+                    mot de passe oublié
+                </button>
+            </Grid>
+        </>
+    )
+}
 
-export default Login;
+export default Login
